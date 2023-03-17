e5e619fc68f2a09d70f6924654807ad36920d9d5
wonwoochoi <onechoo1106@gmail.com>
2023-03-17 21:48:45 +1000

    extracted view finished

--- AssignmentMAD/Views/ExtractedView.swift
+++ AssignmentMAD/Views/ExtractedView.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ExtractedView:View{
+    @Binding var task:[String]
+    var body:some View{
+        HStack{
+            Text(task[0])
+            Spacer()
+            Image(systemName: task[2])//systemName: "checkmark")
+        }.onTapGesture {
+            if(task[2]=="checkmark"){
+                task[2]==""
+            }else{
+                task[2]="checkmark"
+            }
+        }
+    }
+}
