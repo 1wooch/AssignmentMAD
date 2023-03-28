ed7451dc8114cfffbdfc398fc430a2f1ebce9f1a
wwc <onechoo1106@gmail.com>
2023-03-28 17:07:26 +1000

    model side done

---
+++ AssignmentMAD/Model/SwiftUIView.swift
@@ -0,0 +1,61 @@
+//
+//  SwiftUIView.swift
+//  AssignmentMAD
+//
+//  Created by Wonwoo Choi on 28/3/2023.
+//
+
+import Foundation
+
+struct checkList:Hashable,Codable{
+    var listName:String
+    var checkListDetail:[[checkListDetailitem]]//
+}
+struct checkListDetailitem:Hashable,Codable{
+    var name:String
+    var check:Bool
+}
+struct DataModel:Codable{
+    var lists:[checkList]
+    var title:String
+    init(){
+        lists=[]
+        title=""
+        load()
+    }
+    mutating func load(){
+        guard let url=getFile(),
+              let data = try? Data(contentsOf: url),
+              let datamodel=try? JSONDecoder().decode(DataModel.self, from: data)else{
+            self.lists=testList
+            self.title=testTitle
+            return
+        }
+    }
+    func save(){
+        guard let url=getFile(),
+              let data=try? JSONEncoder().encode(self)
+        else{
+            return
+        }
+        try? data.write(to: url)
+    }
+}
+
+var testTitle="Checklist"
+var testList=[
+    checkList(listName: "first check list", checkListDetail:[[]] )
+]
+
+
+
+func getFile()->URL?{
+    let filename="checkList.json"
+    let fm = FileManager.default
+    guard let url=fm.urls(for: .documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask).first else{
+        return nil
+    }
+    return url.appendingPathComponent(filename)
+}
+
+
--- AssignmentMAD/Views/ContentView.swift
+++ AssignmentMAD/Views/ContentView.swift
@@ -6,33 +6,10 @@
 //
 
 import SwiftUI
-/*
-struct ContentView: View {
-    @State var GroceryList=[["Milk",""],["Sugar",""],["Bread",""],["Cheese",""]]
-    
-    var body:some View{
-        
-        //VStack(alignment: .leading){
-        NavigationView{
-            List{
-                    ForEach($GroceryList,id:\.self){
-                        $grocery in
-                        NavigationLink(destination: ListDetailView(item:grocery)){
-                            ExtractedView(item: $grocery)
-                        }
-                    }
-            }.navigationTitle("Grocery Detail")
-            Spacer()
-        }.padding()
-    }
-}
-*/
 
 struct MileStone1View:View{
     @State var goceryModel : [GroceryList]
-   // @State var GroceryList2=[["Milk","y"],["Sugar","n"],["Bread","y"],["Cheese","n"]]
-    //change this into struct struct GroeceryList {var name:String, var check:Bool }
-    
+
     var body:some View{
         NavigationStack{
             List{
@@ -48,23 +25,4 @@ struct MileStone1View:View{
         
     }
 }
-//
-//struct MileStone1ModelView:View{
-//    @State var GroceryList:[GroceryList]
-//    var body:some View{
-//        NavigationStack{
-//            List{
-//                ForEach($gro) { list in
-//                    /*@START_MENU_TOKEN@*/Text(list.name)/*@END_MENU_TOKEN@*/
-//                }
-//            }
-//        }
-//    }
-//}
 
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        MileStone1View(goceryModel:groceryInfo)
-    }
-}
